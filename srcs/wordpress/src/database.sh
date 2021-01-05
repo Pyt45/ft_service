8974f58dbe0ec88f5807330b5da21b54cd2ce96d
Ayoub <49948548+Pyt45@users.noreply.github.com>
2021-01-05 22:07:24 +0100

    Update database.sh

--- srcs/wordpress/src/database.sh
+++ srcs/wordpress/src/database.sh
@@ -2,6 +2,6 @@
 
 echo "create database wordpress;" | mysql -u root
 echo "create user admin" | mysql -u root
-echo "grant all privileges on wordpress.* to 'admin'@'localhost' identified by 'admin';" | mysql -u root
+echo "grant all privileges on wordpress.* to 'admin'@'%' identified by 'admin';" | mysql -u root
 echo "flush privileges;" | mysql -u root
 echo "quit" | mysql -u root
