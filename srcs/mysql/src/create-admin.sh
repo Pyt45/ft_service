028b3674dfe3d9960f92109269b59cc500a98635
Ayoub <49948548+Pyt45@users.noreply.github.com>
2021-01-06 15:37:19 +0100

    Update create-admin.sh

--- srcs/mysql/src/create-admin.sh
+++ srcs/mysql/src/create-admin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+echo "create user 'admin'@'%' identified by 'admin';" | mysql -u root
 echo "create database wordpress;" | mysql -u root
-echo "create user admin@% identified by 'admin';" | mysql -u root
-echo "grant all privileges on *.* to admin@%;" | mysql -u root
+echo "grant all privileges on *.* to 'admin'@'%';" | mysql -u root
 echo "flush privileges;" | mysql -u root
 echo "quit" | mysql -u root
