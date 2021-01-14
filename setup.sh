ec69e03e0cdf53cc4d1a1ec115926cef29c13184
Ayoub <49948548+Pyt45@users.noreply.github.com>
2021-01-14 22:56:19 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# author: ayoub
+# 42 Project: ft_services
 printf "\n\n\033[0;32m ███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████\033[0m\n"
 printf "\033[0;32m █░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█████████████████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█\033[0m\n"
 printf "\033[0;32m █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█████████████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█\033[0m\n"
@@ -13,14 +15,6 @@ printf "\033[0;32m █░░▄▀░░█████████████�
 printf "\033[0;32m █░░░░░░█████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█████░░░░░░█████░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█\033[0m\n"
 printf "\033[0;32m ███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████\033[0m\n\n\n"
 
-# Install metallb
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# # On first install only
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# # ConfigMap
-# kubectl apply -f ./srcs/metallb-config.yaml
-
 arr=(
     mysql
     phpmyadmin
@@ -31,31 +25,156 @@ arr=(
     grafana
 )
 
-# function install_kube(){
-#     which brew &> /dev/null
-#     if [ !]
-# }
+function install_kube(){
+    dir_goinfre=/goinfre/$USER
+    docker_dest=$dir_goinfre/.docker
+    kube_dest=$dir_goinfre/.kube
+    minikube_dest=$dir_goinfre/.minikube
+    brew_home=$HOME/.brew
+    brew_dest=$dir_goinfre/.brew
+
+    pre_config_start=`date +%s`
+    # Install Brew
+    if ! $(which -s brew &> /dev/null); then
+        echo -e "\033[1;32mBrew Not Installed, Installing...\033[0m"
+        # Install brew
+		rm -rf $HOME/.brew
+        git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
+        echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc
+        source $HOME/.zshrc
+    else
+      echo -e "\033[0;32mBrew Allredy Installed\033[0m"
+    fi
+    mv $brew_home $brew_dest
+    ln -s $brew_dest
+    echo -e "\033[0;32mUpdating Brew...\033[0m"
+    brew update &> /dev/null
+    # KUBECTL
+    if ! $(which -s kubectl &> /dev/null); then
+        echo -e "\033[1;32mKubectl not Installed, Installing...\033[0m"
+        # Install kubectl
+        brew install kubectl
+    fi
+    # MINIKUBE
+    if ! $(which -s minikube &> /dev/null); then
+        echo -e "\033[1;32mMinikube not Installed, Installing...\033[0m"
+        # Install Minikube 
+        brew install minikube
+    fi
+    # DOCKER
+    if ! $(which -s docker &> /dev/null); then
+      echo -e "\033[1;32mDocker not Installed, Installing...\033[0m"
+      # Install Docker
+      brw install docker
+    fi
+    rm -rf .docker
+    rm -rf .minikube
+    rm -rf $docker_dest
+    rm -rf $dir_minikube
+    rm -rf $kube_dest
+    mkdir $docker_dest
+	mkdir $kube_dest
+    mkdir $minikube_dest
+	ln -s $minikube_dest
+    ln -s $docker_dest
+    ln -s $kube_dest
+    pre_config_end=`date +%s`	
+	runtime=$((pre_config_end-pre_config_start))
+	echo -e "\033[0;32mPre-config done - $runtime seconds)\033[0m"
+}
+
+function set_up() {
+    # Install metallb
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+    # On first install only
+    if ! $(kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"); then
+        echo -e "\033[1;32mSecretKey Alredy Exist\033[0m"
+    fi
+    # ConfigMap
+    kubectl apply -f ./srcs/metallb-config.yaml
+    minikube delete &> /dev/null
+    echo "\033[0;32mCreating VirtualMachine\033[0m"
+    if ! $(minikube start --vm-driver=virtualbox --disk-size=20g --memory=4g --cpus=4 &> /dev/null); then
+        echo -e "\033[1;32mError Creating VirtualMachine\033[0m"
+    fi
+    if ! $(minikube status &> /dev/null); then
+        sleep 10
+        if ! $(minikube status &> /dev/null); then
+            echo "\033[1;32mMinikube is not Running...\033[0m"
+            exit 1
+        fi
+    else
+        echo "\033[0;32mVirtualMachine was created successfully\033[0m"
+    fi
+    eval $(minikube docker-env)
+    if [ $? -eq 0 ]; then
+        echo -e "\033[1;32ENVIRMENT EXPORTED\033[0m"
+    else
+        echo -e "\033[0;32mERROR SETING ENVIREMENT\033[0m"
+    fi
+}
 
 function start_up(){
-    if [[ $1 == "delete" ]]; then
-        echo "-------delete-------"
+    if [[ $1 == "install" ]]; then
+        install_kube
+    elif [[ $1 == "setup" ]]; then
+        set_up
+    elif [[ $1 == "delete" ]]; then
+        echo -e "\033[1;32m-------delete-------\033[0m"
         for k in "${arr[@]}"; do
             kubectl delete -f ./srcs/$k-deployment.yaml
         done
-        echo "------- done -------"
+        echo -e "\033[1;32m------- All Services Deleted -------\033[0m"
     elif [[ $1 == "start" ]]; then
-        echo "-------create-------"
+        start_time=`data +%s`
+        echo "\033[0;32m-------Starting-------\033[0m"
         for i in "${arr[@]}"; do
             docker build -t $i ./srcs/$i
         done
+        sleep 2
         # Create secret for mysql
         kubectl apply -f ./srcs/secret.yaml
+        sleep 2
         # Create volumes
         kubectl apply -f ./srcs/PersistentVolume.yaml
+        sleep 2
         for j in "${arr[@]}"; do
             kubectl apply -f ./srcs/$j-deployment.yaml
         done
-        echo "------- done -------"
+        sleep 4
+        echo "\033[0;32m------- Done -------\033[0m"
+        end_time=`date +%s`
+        # FT_SERVICES_IP=`kubectl get svc | grep EXTERNAL_IP | awk '{print $4}'` # Error
+        runtime=$((end_time-start_time))
     fi
 }
+
+function ft_services() {
+echo "✅		ft_services deployment done (Hourra ! - $runtime seconds)"
+echo " 
+Minikube IP is : $MINIKUBE_IP - Type 'minikube dashboard' for dashboard
+================================================================================
+LINKS:
+	nginx:			https://$FT_SERVICES_IP (or http)
+	wordpress:		http://$FT_SERVICES_IP:5050
+	phpmyadmin:		http://$FT_SERVICES_IP:5000
+	grafana:		http://$FT_SERVICES_IP:3000
+OTHERS:
+	nginx:			ssh admin@$FT_SERVICES_IP -p 22
+	ftps:			USING FILEZILLA
+	
+ACCOUNTS:			(username:password)
+	ssh:			$SSH_USERNAME:$SSH_PASSWORD (port 22)
+	ftps:			$FTPS_USERNAME:$FTPS_PASSWORD (port 21)
+	database:		$DB_USER:$DB_PASSWORD (sql / phpmyadmin)
+	grafana:		admin:admin
+	wordpress:      admin:admin (Admin)
+TEST PERSISTENT MYSQL/INFLUXDB:
+	kubectl exec -it \$(kubectl get pods | grep mysql | cut -d\" \" -f1) -- /bin/sh -c \"kill 1\"
+	kubectl exec -it \$(kubectl get pods | grep influxdb | cut -d\" \" -f1) -- /bin/sh -c \"kill 1\"
+"
+}
+
 start_up $1
+ft_services
