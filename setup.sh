b2770334a462444f2732e6a01904c6f9593a3807
aaqlzim <ayoub1234aqlzim@gmail.com>
2021-01-14 18:58:53 +0100

    almost_done

--- setup.sh
+++ setup.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
-
-# echo " \033[1;31m_______ _________   _______  _______  _______          _________ _______  _______  _______  "
-# echo "(  ____ \\\\\__   __/  (  ____ \(  ____ \(  ____ )|\     /|\__   __/(  ____ \(  ____ \(  ____ \ "
-# echo "| (    \/   ) (     | (    \/| (    \/| (    )|| )   ( |   ) (   | (    \/| (    \/| (    \/ "
-# echo "| (__       | |     | (_____ | (__    | (____)|| |   | |   | |   | |      | (__    | (_____  "
-# echo "|  __)      | |     (_____  )|  __)   |     __)( (   ) )   | |   | |      |  __)   (_____  ) "
-# echo "| (         | |           ) || (      | (\ (    \ \_/ /    | |   | |      | (            ) | "
-# echo "| )         | |     /\____) || (____/\| ) \ \__  \   /  ___) (___| (____/\| (____/\/\____) | "
-# echo "|/          )_(_____\_______)(_______/|/   \__/   \_/   \_______/(_______/(_______/\_______) "
-# echo "              (_____)                                                                        \033[0m"
-
-# printf "\n\n\033[1;31m    █▀▀ ▀█▀    █▀ █▀▀ █▀█ █░█ █ █▀▀ █▀▀ █▀\033[0m\n"
-# printf         "\033[1;31m    █▀░ ░█░ ▀▀ ▄█ ██▄ █▀▄ ▀▄▀ █ █▄▄ ██▄ ▄█\033[0m\n\n\n"
-
-
 printf "\n\n\033[0;32m ███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████\033[0m\n"
 printf "\033[0;32m █░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░██░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█\033[0m\n"
 printf "\033[0;32m █░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█\033[0m\n"
@@ -26,13 +11,16 @@ printf "\033[0;32m █░░▄▀░░█████████████�
 printf "\033[0;32m █░░▄▀░░█████████████░░▄▀░░█████░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░░░░░█░░░░▄▀▄▀▄▀░░░░█░░░░▄▀░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░░░░░░░░░▄▀░░█\033[0m\n"
 printf "\033[0;32m █░░▄▀░░█████████████░░▄▀░░█████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░███░░░░▄▀░░░░███░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█\033[0m\n"
 printf "\033[0;32m █░░░░░░█████████████░░░░░░█████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█████░░░░░░█████░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█\033[0m\n"
-printf "\033[0;32m ███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████\033[0m\n\n\n"0
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# # # On first install only
-# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+printf "\033[0;32m ███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████\033[0m\n\n\n"
 
-# kubectl apply -f ./srcs/metallb-config.yaml
+# Install metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# ConfigMap
+kubectl apply -f ./srcs/metallb-config.yaml
+
 arr=(
     mysql
     phpmyadmin
@@ -42,6 +30,11 @@ arr=(
     influxdb
     grafana
 )
+
+# function install_kube(){
+#     which brew &> /dev/null
+#     if [ !]
+# }
 
 function start_up(){
     if [[ $1 == "delete" ]]; then
@@ -66,85 +59,3 @@ function start_up(){
     fi
 }
 start_up $1
-
-# echo "====================================="
-# echo "==---------------------------------=="
-# echo "== Grafana & InfluxDB Installation =="
-# echo "==        For Raspberry Pi         =="
-# echo "==                                 =="
-# echo "==        From SIYTEK.COM          =="
-# echo "==                                 =="
-# echo "==   Electronics Do It Yourself!   =="
-# echo "==---------------------------------=="
-# echo "====================================="
-
-# echo " "
-# echo "Installing Grafana..."
-
-# wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-# echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-
-# apt update && sudo apt install -y grafana
-
-# systemctl unmask grafana-server.service
-# systemctl start grafana-server
-# systemctl enable grafana-server.service
-
-# echo " "
-# echo "Installing InfluxDB..."
-
-# wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-# source /etc/os-release
-# echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-
-# apt-get update && apt-get install influxdb
-
-# sudo systemctl unmask influxdb.service
-# sudo systemctl start influxdb
-# sudo systemctl enable influxdb.service
-
-# echo " "
-# read -p "Installation complete, press ENTER to continue with setup..." null
-
-# echo "========================="
-# echo "= Set up a new database ="
-# echo "========================="
-# echo " "
-# echo "Enter your desired values (no spaces)..."
-
-# read -p "Enter a name for the database: " dbname
-# read -p "Enter a username for the database: " usrname
-# read -p "Enter a password for the database: " pssword
-
-# echo "Creating database..."
-# influx -execute "create database $dbname"
-# echo "Setting database..."
-# influx -execute "use $dbname"
-# echo "Setting privileges..."
-# influx -execute "create user "$usrname" with password '"$pssword"' with all privileges"
-# influx -execute "grant all privileges on $dbname to $usrname"
-# echo " "
-# influx -execute "show users"
-# echo "...DONE!"
-# echo " "
-# echo "======================="
-# echo "How to setup Grafana..."
-# echo "======================="
-# echo "Step 1: Point your browser to http://<your-ip>:3000"
-# echo "Step 2: Log in with the default Grafana" 
-# echo "        user: admin"
-# echo "        pass: admin"
-# echo "Step 3: Change default Grafana login"
-# echo "Step 4: Add data source, choose InfluxDB"
-# echo "step 5: Set HTTP"
-# echo "        URL: http://<your-ip>:8086"
-# echo "Step 6: Set InfluxDB Details..."
-# echo "        Database: $dbname"
-# echo "        User: $usrname"
-# echo "        Password: $pssword"
-# echo "        HTTP Method: GET"
-# echo "Step 7: Click Save & Test"
-# echo "========================="
-# echo " "
-# echo "Thanks for using the SIYTEK.COM Grafana & InfluxDB easier installation script!"
-# echo "Come visit me @ https://www.siytek.com"
