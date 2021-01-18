235d750c012718dac1b737d2ecb83159c11f1216
aaqlzim <ayoub1234aqlzim@gmail.com>
2021-01-18 09:39:00 +0100

    done

--- srcs/grafana/src/entry-point.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/usr/bin/telegraf --config /etc/telegraf/telegraf.conf --config-directory /etc/telegraf.conf.d
-/grafana/bin/grafana-server --config "/grafana/conf/defaults.ini" -homepath "/grafana"
-tail -f /dev/null
--- srcs/influxDb/src/create-admin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-influx -execute "CREATE USER \"admin\" WITH PASSWORD 'admin' WITH ALL PRIVILEGES"
-influx -username admin -password admin -execute "CREATE DATABASE influxdb"
-/usr/bin/supervisord
